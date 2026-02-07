bb7fb66abd2755e5ecd0b64bb233a69f4a1e438c
Chasen Bettinger <bettingerchasen@gmail.com>
2026-02-07 13:22:45 -0500

    add oh-my-zsh plugins

--- init.sh
+++ init.sh
@@ -26,6 +26,23 @@ link_file() {
 link_file "$ZSHRC_SOURCE" "$ZSHRC_TARGET"
 link_file "$P10K_SOURCE" "$P10K_TARGET"
 
+# Install oh-my-zsh custom plugins
+PLUGINS_FILE="$REPO_DIR/oh-my-zsh-plugins/plugins.txt"
+CUSTOM_PLUGINS_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins"
+
+if [ -f "$PLUGINS_FILE" ]; then
+    while read -r name url; do
+        [ -z "$name" ] && continue
+        dest="$CUSTOM_PLUGINS_DIR/$name"
+        if [ -d "$dest" ]; then
+            echo "Plugin $name already installed, pulling latest..."
+            git -C "$dest" pull --quiet
+        else
+            echo "Cloning plugin $name..."
+            git clone --quiet "$url" "$dest"
+        fi
+    done < "$PLUGINS_FILE"
+fi
 
 # Create .zshrc.local if it doesn't exist
 if [ ! -f "$HOME/.zshrc.local" ]; then
